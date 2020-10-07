b89a2709d6bfbdf709946b09dd1d0b32eb4c3b4a
Artem Popov <aptem336@gmail.com>
2020-10-07 19:18:54 +0800

    personal area

--- src/main/java/controller/PersonalArea.java
+++ src/main/java/controller/PersonalArea.java
@@ -1,29 +1,42 @@
 package controller;
 
 import model.Person;
+import model.Users;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
+import javax.security.enterprise.SecurityContext;
 
 @Named
 @Stateless
 public class PersonalArea {
     @Inject
     private EntityManager em;
-    private Person person;
+    @Inject
+    private SecurityContext securityContext;
+    private Users users;
 
-    public Person getPerson() {
-        return person;
+    @PostConstruct
+    private void postConstruct() {
+        users = em.find(Users.class, securityContext.getCallerPrincipal());
     }
 
-    public void setPerson(Person person) {
-        this.person = person;
+    public Person getPerson() {
+        if (users.getPerson() == null) {
+            users.setPerson(new Person());
+        }
+        return users.getPerson();
     }
 
     public String save() {
-        em.merge(person);
+        em.merge(users);
         return "flight_filter?faces-redirect=true";
+    }
+
+    public Person.Gender[] getGenderValues() {
+        return Person.Gender.values();
     }
 }
